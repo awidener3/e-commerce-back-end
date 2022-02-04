9ca793182e68ce96ff6259c7db780e3f424312d9
Alex Widener <awidener3@gmail.com>
2022-02-03 23:13:08 -0500

    added CRUD to model routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,71 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// * find all categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findAll({
+			include: { model: Product },
+		});
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findByPk(req.params.id, {
+			include: { model: Product },
+		});
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * create a new category
 router.post('/', (req, res) => {
-  // create a new category
+	try {
+		const newCategory = await Category.create(req.body.category_name);
+		res.status(200).json(newCategory);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+	try {
+		let updatedCategory = await Category.update(
+			{ category_name: req.body.category_name },
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		);
+		res.status(200).json(updatedCategory);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+	try {
+		let deletedCategory = await Category.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		res.status(200).json(deletedCategory);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,74 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// * get all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+	// be sure to include its associated Product data
+	try {
+		const tagData = await Tag.findAll({
+			include: { model: Product },
+		});
+		res.status(200).json(tagData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * get a single tag by its `id`
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+	// be sure to include its associated Product data
+	try {
+		const tagData = await Tag.findByPk(req.params.id, {
+			include: { model: Product },
+		});
+		res.status(200).json(tagData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+	try {
+		const newTag = await Tag.create(req.body.tag_name);
+		res.status(200).json(newTag);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+	try {
+		let updatedUser = await Tag.update(
+			{ tag_name: req.body.tag_name },
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		);
+		res.status(200).json(updatedUser);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// * delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+	try {
+		let deletedTag = await Tag.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		res.status(200).json(deletedTag);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
